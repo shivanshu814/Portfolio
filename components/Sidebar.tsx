91e3071476d7a4a5c225c0eb5ac4153b9120dcf5
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-03-21 20:08:08 +0530

    TailwindCSS

---
+++ components/Sidebar.tsx
@@ -0,0 +1,22 @@
+/** @format */
+
+import React from 'react';
+
+const Sidebar = () => {
+	return (
+		<div>
+			<img
+				src='https://media.licdn.com/dms/image/C4D03AQFEsyMECbhHfA/profile-displayphoto-shrink_800_800/0/1667972114483?e=2147483647&v=beta&t=H8xxDOgEyLpi_4jhdIiFRE5M0cQuh8f8r_S8yOokz-U'
+				alt='user avatar'
+			/>
+			<h3>
+				<span>Shivanshu</span>Pathak
+			</h3>
+			<p>Web Developer</p>
+			<p>Android Developer</p>
+			<p>Download Resume</p>
+		</div>
+	);
+};
+
+export default Sidebar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
