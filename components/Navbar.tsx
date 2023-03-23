88cae5e54cfd2058f6b5975b5155b09e169faf4f
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-03-23 11:05:51 +0530

    useState Working

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,34 +3,28 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 const Navbar = () => {
-	// const [activeItem, setActiveItem] = useState<String>('');
+	const [activeItem, setActiveItem] = useState<string>('');
 
 	return (
 		<div>
-			{/* <span className='font-bold'>{activeItem}</span>
+			<span className='font-bold'>{activeItem}</span>
 			<div>
 				{activeItem !== 'About' && (
 					<Link href='/'>
-						<a>
-							<span>About</span>
-						</a>
+						<span>About</span>
 					</Link>
 				)}
 				{activeItem !== 'Projects' && (
 					<Link href='/projects'>
-						<a>
-							<span>Projects</span>
-						</a>
+						<span>Projects</span>
 					</Link>
 				)}
 				{activeItem !== 'Resume' && (
 					<Link href='/resume'>
-						<a>
-							<span>Resume</span>
-						</a>
+						<span>Resume</span>
 					</Link>
 				)}
-			</div> */}
+			</div>
 		</div>
 	);
 };
