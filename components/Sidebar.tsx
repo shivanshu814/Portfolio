286f0e40c5fdacfb6b19d2faf2ba35d0df3a59d8
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-03-23 10:21:28 +0530

    Navbar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,8 +14,8 @@ const Sidebar = () => {
 				alt='user avatar'
 				className='w-32 h-32 mx-auto rounded-full'
 			/>
-			<h1 className='my-4 text-3xl tracking-wider font-delicious'>
-				<span>Shivanshu</span> Pathak
+			<h1 className='my-4 text-3xl tracking-wider text-white font-delicious'>
+				<span className='text-red-500'>Shivanshu</span> Pathak
 			</h1>
 			<p className='px-2 py-1 my-3 bg-orange-400 rounded-full'>Web Developer</p>
 			<p className='px-2 py-1 my-3 bg-gray-200 rounded-full'>
@@ -32,7 +32,7 @@ const Sidebar = () => {
 			</a>
 
 			{/* social media */}
-			<div className='flex justify-around w-9/12 mx-auto my-5 text-black-700 md:w-full '>
+			<div className='flex justify-around w-9/12 mx-auto my-5 text-green-700 md:w-full'>
 				<a href='https://www.linkedin.com/in/shivanshu-pathak-6611b5228/'>
 					<FaLinkedin className='w-6 h-6' cursor-pointer />
 				</a>
@@ -52,15 +52,17 @@ const Sidebar = () => {
 				style={{ marginLeft: '-0.7rem', marginRight: '-0.7rem' }}>
 				<div className='flex items-center justify-center'>
 					<GoLocation className='w-4 h-4 space-x-2' />
-					<span>Greater Noida, Uttar Pradesh 201310</span>
+					<span>Greater Noida</span>
 				</div>
 				<p className='my-2'>shivanshu814pathak@gmail.com</p>
-				<p className='my-2'>+91 9893628988 / +91 8595438920</p>
+				<p className='my-2'>9893628988 / 8595438920</p>
 			</div>
 
 			{/* contact */}
-			<button className='w-8/12 px-5 py-2 my-1 text-white rounded-full bg-gradient-to-r from-orange-500 to-green-500'>
-				Email me
+			<button
+				className='w-8/12 px-5 py-2 my-1 text-white rounded-full bg-gradient-to-r from-orange-500 to-green-500 focus:outline-none'
+				onClick={() => window.open('mailto:shivanshu814pathak@gmail.com')}>
+				Email Me
 			</button>
 			{/* Theme */}
 			<button className='w-8/12 px-5 py-2 my-1 text-white rounded-full bg-gradient-to-r from-orange-500 to-green-500'>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
@@ -7,10 +8,11 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<div className='grid grid-cols-12 gap-6 px-5 lg:px-48 my-14'>
-			<div className='col-span-12 p-3 text-center bg-white lg:col-span-3 rounded-2xl'>
+			<div className='col-span-12 p-3 text-center bg-black lg:col-span-3 rounded-3xl'>
 				<Sidebar></Sidebar>
 			</div>
-			<div className='col-span-12 bg-white lg:col-span-9 rounded-2xl'>
+			<div className='col-span-12 bg-white lg:col-span-9 rounded-3xl'>
+				<Navbar></Navbar>
 				<Component {...pageProps} />
 			</div>
 		</div>
