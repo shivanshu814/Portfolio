8f88ff3b5871d1da549a3cc8e515d098f09a2ae5
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-03-23 14:53:06 +0530

    Route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,51 @@
 /** @format */
 
-import React, { useState } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const NavItem: FunctionComponent<{
+	activeItem: string;
+	setActiveItem: Function;
+	name: string;
+	route: string;
+}> = ({ activeItem, name, route, setActiveItem }) => {
+	return activeItem !== name ? (
+		<Link href={route} legacyBehavior>
+			<span onClick={() => setActiveItem(name)}>{name}</span>
+		</Link>
+	) : null;
+};
+
 const Navbar = () => {
 	const [activeItem, setActiveItem] = useState<string>('');
 
+	const { pathname } = useRouter();
+
+	useEffect(() => {
+		if (pathname === '/') setActiveItem('About');
+		if (pathname === '/projects') setActiveItem('Projects');
+		if (pathname === '/resume') setActiveItem('Resume');
+	}, []);
 	return (
 		<div>
-			<span className='font-bold'>{activeItem}</span>
-			<div>
-				{activeItem !== 'About' && (
-					<Link href='/' legacyBehavior>
-						<span>About</span>
-					</Link>
-				)}
-				{activeItem !== 'Projects' && (
-					<Link href='/projects' legacyBehavior>
-						<span>Projects</span>
-					</Link>
-				)}
-				{activeItem !== 'Resume' && (
-					<Link href='/resume' legacyBehavior>
-						<span>Resume</span>
-					</Link>
-				)}
+			<span className='font-bold text-green'>{activeItem}</span>
+			<div className='flex space-x-3 text-red-600 cursor-pointer font-lg'>
+				<NavItem
+					activeItem={activeItem}
+					setActiveItem={setActiveItem}
+					name='About'
+					route='/'></NavItem>
+				<NavItem
+					activeItem={activeItem}
+					setActiveItem={setActiveItem}
+					name='Projects'
+					route='/projects'></NavItem>
+				<NavItem
+					activeItem={activeItem}
+					setActiveItem={setActiveItem}
+					name='Resume'
+					route='/resume'></NavItem>
 			</div>
 		</div>
 	);
